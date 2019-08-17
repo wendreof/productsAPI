7eea1d5e75edad5fd6b15ab5008a190187611b44
Wendreo <wendreolf@gmail.com>
2019-08-17 15:37:49 -0300

    :rocket:

--- node/src/app.js
+++ node/src/app.js
@@ -2,9 +2,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//Conecta ao banco
+mongoose.connect('mongodb+srv://wendreof:thebestsandra3@wendreof-omnob.azure.mongodb.net/test', {
+    useNewUrlParser: true,
+})
+
+//Carrega os Models
+const Product = require('./models/product')
 
 //Carrega as Rotas
 const indexRoute = require('./routes/index-route')
--- node/src/controllers/product-controller.js
+++ node/src/controllers/product-controller.js
@@ -1,7 +1,47 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product')
+
+exports.get = (req, res, next) => {
+    Product
+        .find({
+            active: true
+        }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .find({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
 exports.post = (req, res, next) => {
-    res.status(201).send(req.body);
+    var product = new Product(req.body);
+    product
+        .save()
+        .then(x => {
+            res.status(201).send({
+                message: 'Produto cadastrado com scuesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao cadastrar produto!',
+                data: e
+            });
+        });
 };
 
 exports.put = (req, res, next) => {
