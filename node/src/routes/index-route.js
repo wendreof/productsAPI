bc389e4670c2e0f4579ab40e9200dc7ad1780670
Wendreo <wendreolf@gmail.com>
2019-08-24 13:59:11 -0300

    New API version :rocket:

--- node/src/routes/index-route.js
+++ node/src/routes/index-route.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store API @wendreo",
-        version: "0.0.3"
+        version: "0.1.1"
     })
 })
 
